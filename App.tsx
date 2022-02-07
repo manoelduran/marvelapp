3e9d43b209892c7c1fdcf9d0b9a9a305ee1479cb
Manoel Duran <manoel.duran@hotmail.com>
2022-02-06 21:24:18 -0300

    app pronto para iniciar

--- App.tsx
+++ App.tsx
@@ -1,32 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+import { useFonts, Poppins_400Regular, Poppins_500Medium } from '@expo-google-fonts/poppins';
 import React from 'react';
 import md5 from 'js-md5';
 const { PUBLIC_KEY } = process.env;
 const { PRIVATE_KEY } = process.env;
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
+import { useTheme } from 'styled-components/native';
+import { SignIn } from '@src/screens/SignIn';
+
 
 export default function App() {
-  async function GetCharacters() {
-    const timestamp = Number(new Date())
-    const hash = md5.create()
-    hash.update(timestamp + PRIVATE_KEY + PUBLIC_KEY)
-
-    const response = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${PUBLIC_KEY}&hash=${hash.hex()}`)
-    const listofCharacters = await response.json();
+  const theme = useTheme();
+  const [fontsLoaded] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium
+  });
+  if (!fontsLoaded) {
+    return <AppLoading />
   }
+  // async function GetCharacters() {
+  //   const timestamp = Number(new Date())
+  //   const hash = md5.create()
+  //   hash.update(timestamp + String(PRIVATE_KEY) + String(PUBLIC_KEY))
+
+  //   const response = await fetch(`https://gateway.marvel.com/v1/public/characters?ts=${timestamp}&orderBy=name&limit=10&apikey=${PUBLIC_KEY}&hash=${hash.hex()}`)
+  //   const listofCharacters = await response.json();
+  // }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <StatusBar
+        translucent
+        backgroundColor={theme.COLORS.ALERT_50}
+      />
+      <SignIn />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+Container,
+Title
+} from './styles';
+
+export function SignIn(){
+    return (
+        <Container>
+            <Title>BORA INICIAR O APP</Title>
+        </Container>
+    );
+}
